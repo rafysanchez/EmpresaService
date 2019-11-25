a24b199ea8e4d039e4a7b1be87070b6758bf61f3
Rafael Portal <rafael.portal@bbce.local>
2019-11-25 12:14:16 -0300

    ajustes iiciais

---
+++ test/EmpresaAPI.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmpresaAPI.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
